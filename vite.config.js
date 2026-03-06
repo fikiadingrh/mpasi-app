d35bf98d09eddb4915b11e35da19e7802d795618
fikiabdingrh <70275345+fikiadingrh@users.noreply.github.com>
2026-03-06 10:35:01 +0700

    Remove base and build options from Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    rollupOptions: {
-      input: './index.html'
-    }
-  }
 })
